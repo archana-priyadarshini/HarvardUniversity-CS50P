2e13a088f3604285b5fc75650400e8fff712437b
Archana Priyadarshini <143933704+archana-priyadarshini@users.noreply.github.com>
2023-09-05 12:40:47 +0000

    Set 2

--- Problem Set 2/twttr/twttr.py
+++ Problem Set 2/twttr/twttr.py
@@ -1,6 +1,6 @@
 # Problem statement: https://cs50.harvard.edu/python/2022/psets/2/twttr/
-input=input("Input: ")
-for i in input:
+inp=input("Input: ")
+for i in inp:
     if i.lower() in ('a','e','i','o','u'):
-        input=input.replace(i,"") 
-print("Output:",input)
+        inp=inp.replace(i,"")
+print("Output:",inp)

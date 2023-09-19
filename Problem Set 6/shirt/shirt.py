bf2f38d0c77d78de2b132497087fe8aec64754c1
Archana Priyadarshini <143933704+archana-priyadarshini@users.noreply.github.com>
2023-09-20 01:22:37 +0530

    Problem Set 6

---
+++ Problem Set 6/shirt/shirt.py
@@ -0,0 +1,26 @@
+#Problem Statement: https://cs50.harvard.edu/python/2022/psets/6/shirt/
+
+import sys,os
+from PIL import Image,ImageOps
+
+if len(sys.argv)<3:
+    sys.exit("Too few command-line arguments ")
+elif len(sys.argv)>3:
+    sys.exit("Too many command-line arguments ")
+
+input=sys.argv[1]
+output=sys.argv[2]
+inp_ext,out_ext=os.path.splitext(input)[1],os.path.splitext(output)[1]
+
+if inp_ext not in ('.jpeg','.jpg','.png') or out_ext not in ('.jpeg','.jpg','.png'):
+    sys.exit("Invalid input")
+if inp_ext!=out_ext:
+    sys.exit("Input and output have different extensions")
+try:
+    input_image=Image.open(input)
+    shirt = Image.open("shirt.png")
+    cropped=ImageOps.fit(input_image, shirt.size)
+    cropped.paste(shirt,shirt)
+    cropped.save(output)
+except FileNotFoundError:
+    sys.exit("Input does not exist")
